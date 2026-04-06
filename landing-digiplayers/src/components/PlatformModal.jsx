bc81858e17632a07648b06657d1787859acc8869
Camilo Cordoba <camilocordobaa@gmail.com>
2026-04-06 12:03:16 -0300

    D

--- landing-digiplayers/src/components/PlatformModal.jsx
+++ landing-digiplayers/src/components/PlatformModal.jsx
@@ -25,7 +25,7 @@ export default function PlatformModal({ platform, onClose }) {
               {platform.mode === "links" && platform.trackingLinks && (
   <div style={{ marginTop: 20 }}>
     <h3 style={{ fontSize: 14, marginBottom: 10 }}>
-      Elegí una opción para registrarte
+      Elegí la provincia en la que Residís
     </h3>
 
     <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
